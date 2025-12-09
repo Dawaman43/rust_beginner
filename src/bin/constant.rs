9e8cd86c02f85026b27d9c79161cf6def94a37b2
Dawaman43 <dawitworkujima@gmail.com>
2025-12-09 18:17:19 +0300

    added constant

---
+++ src/bin/constant.rs
@@ -0,0 +1,7 @@
+//  constant are immutable even if we use mut keyword 
+// and we have to specify the data type for the constants
+
+fn main(){
+    const CONSTANT_TEST : u32= 3;
+    println!("Constant is {CONSTANT_TEST}");
+}
