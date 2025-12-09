e2ddf6b4d0fb1ea2d308811ae9cacf0e436f03d3
Dawaman43 <dawitworkujima@gmail.com>
2025-12-09 19:22:17 +0300

    added src array

---
+++ src/bin/array.rs
@@ -0,0 +1,9 @@
+// array in rust use to store same types in fixed length 
+
+fn main(){
+    let ar = [1,2,3,4];
+    // to print element values from array we will use [element index] or indexing
+
+    println!("The first element in the array is {}", ar[0]);
+    // and we can computation using indexing
+}
